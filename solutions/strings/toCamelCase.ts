3fd34595bc800accd6d836516b48727a88c65233
maxcodingtx <maxcodingtx@gmail.com>
2025-06-10 20:48:52 -0500

    create toCamelCase.ts file and challenge

---
+++ solutions/strings/toCamelCase.ts
@@ -0,0 +1,9 @@
+/*
+Define a function that turns any string into a camelCased string.
+
+
+Inside the function handle the following corner cases:
+  - Empty or null input
+  - Input empty of any characters
+
+*/
