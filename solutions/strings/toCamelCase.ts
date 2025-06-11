d0bde7ebee17465f0006d61b5b3ad25bbf1f1bdc
maxcodingtx <maxcodingtx@gmail.com>
2025-06-10 21:04:57 -0500

    toCamelCase.ts challenge complete.

--- solutions/strings/toCamelCase.ts
+++ solutions/strings/toCamelCase.ts
@@ -7,3 +7,40 @@ Inside the function handle the following corner cases:
   - Input empty of any characters
 
 */
+
+function toCamelCase(input: string): string {
+    // handle empty or null input
+    if (!input) {
+        return "";
+    }
+
+    const words = input.split(" ");
+
+    // Handle empty input case
+    if (words.length === 0) {
+        return "";
+    }
+
+    // First word remains the same, rest have their first letter capitalized
+    const camelCased = words.reduce((acc, word, index) => {
+        if (index === 0) {
+            return word;
+        } else {
+            if (word.length > 0) {
+                return acc + word.charAt(0).toUpperCase() + word.slice(1);
+            }
+            return acc;
+        }
+    }, "");
+
+    return camelCased;
+}
+
+console.log(
+    toCamelCase("test string"), // testString
+    toCamelCase("hello world"), // helloWorld
+    toCamelCase(123), // fails due to 123 not being string
+    toCamelCase(false), // fails due to false not being string
+    toCamelCase(Symbol("symbol")) // fails due to symbol
+     not being string
+);
