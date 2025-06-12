ad0fb84c8cb09b210b922152195ae2cb5d2b77b5
maxcodingtx <maxcodingtx@gmail.com>
2025-06-12 01:37:17 -0500

    complete typeOfFirstArray challenge.

--- solutions/types/typeOfFirstArray.ts
+++ solutions/types/typeOfFirstArray.ts
@@ -2,3 +2,16 @@
 Implement a a generic which captures the type of the first element of an array.
 The type should be inferred from the first element of the array.
 */
+
+type TypeOfFirstArray<T extends any[]> = T extends [infer U, ...any[]]
+    ? U
+    : never;
+
+type arr1 = ["a", "b", "c"];
+type firstElement1 = TypeOfFirstArray<arr1>; // 'a'
+type arr2 = [1, 2, 3];
+type firstElement2 = TypeOfFirstArray<arr2>; // 1
+type arr3 = [true, 3n, true];
+type firstElement3 = TypeOfFirstArray<arr3>; // boolean
+type arr4 = [];
+type firstElement4 = TypeOfFirstArray<arr4>; // never
