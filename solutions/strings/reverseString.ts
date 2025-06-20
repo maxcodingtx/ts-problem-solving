6c0bf5485647b70e8b94239aaaa035b575511831
maxcodingtx <maxcodingtx@gmail.com>
2025-06-19 23:34:29 -0500

    complete reverseString.ts challenge.

--- solutions/strings/reverseString.ts
+++ solutions/strings/reverseString.ts
@@ -2,3 +2,16 @@
   Write a function that reverses a string. 
   The input string is given a normal string (str).
 */
+
+function reverseString(str: string): string {
+  let reversedString = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    console.log(str[i], i);
+    reversedString += str[i];
+  }
+
+  return reversedString;
+}
+
+console.log(reverseString("eggsdee"));
