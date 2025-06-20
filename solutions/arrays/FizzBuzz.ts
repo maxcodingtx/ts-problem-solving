dbaad89d9cf738d254d057e278f15826be55c203
maxcodingtx <maxcodingtx@gmail.com>
2025-06-19 23:24:11 -0500

    complete FizzBuzz challenge in FizzBuzz.ts file.

--- solutions/arrays/FizzBuzz.ts
+++ solutions/arrays/FizzBuzz.ts
@@ -7,3 +7,22 @@ Given an integer n, return an array (result) where:
   result[i] == i (as a string) if none of the above conditions are true.
 
 */
+
+function FizzBuzz(n: number): (number | string)[] {
+  let result: (number | string)[] = [];
+
+  for (let i = 0; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+console.log(FizzBuzz(15));
