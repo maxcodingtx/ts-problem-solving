30ab74f4817998787adac2b2e8b5d522ae3194e4
maxcodingtx <maxcodingtx@gmail.com>
2025-06-19 23:12:42 -0500

    create FizzBuzz challenge.

---
+++ solutions/arrays/FizzBuzz.ts
@@ -0,0 +1,9 @@
+/*
+Given an integer n, return an array (result) where:
+
+  result[i] == "FizzBuzz" if i is divisible by 3 and 5.
+  result[i] == "Fizz" if i is divisible by 3.
+  result[i] == "Buzz" if i is divisible by 5.
+  result[i] == i (as a string) if none of the above conditions are true.
+
+*/
