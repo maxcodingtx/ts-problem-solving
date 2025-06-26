2844b853586c6c62a9a82ae18c07c3d43821cfdc
maxcodingtx <maxcodingtx@gmail.com>
2025-06-25 21:06:25 -0500

    create implementReadonly.ts file and challenge.

---
+++ solutions/types/implementReadonly.ts
@@ -0,0 +1,6 @@
+/*
+Implement the built-in Readonly<T> generic without using it.
+
+Construct a type with all properties of T set to readonly,
+meaning the properties of the constructed type cannot be reassigned.
+*/
