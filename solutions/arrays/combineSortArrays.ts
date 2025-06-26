7d84c206c5d16e94415c59e266cc6224d66e34fd
maxcodingtx <maxcodingtx@gmail.com>
2025-06-25 20:55:24 -0500

    complete combineSortArray.ts challenge.

--- solutions/arrays/combineSortArrays.ts
+++ solutions/arrays/combineSortArrays.ts
@@ -2,3 +2,21 @@
 Given 2 arrays of numbers, combine them and 
 sort them (without using concat() or sort() methods)
 */
+
+// sort array using bubble sort
+function sortCombinedArray(arr: number[]): number[] {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // Array destructuring for a cleaner swap
+      }
+    }
+  }
+  return arr;
+}
+
+function combineSortArrays(arr1: number[], arr2: number[]): number[] {
+  const combinedArrays = [...arr1, ...arr2];
+  const combinedSortedArray = sortCombinedArray(combinedArrays);
+  return combinedSortedArray;
+}
