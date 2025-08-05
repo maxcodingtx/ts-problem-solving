4a0fc6ca3d6a831a03f848b38d5e774f2101dbf1
maxcodingtx <maxcodingtx@gmail.com>
2025-08-04 19:13:57 -0500

    debounce.ts challenge complete.

--- solutions/other/debounce.ts
+++ solutions/other/debounce.ts
@@ -3,3 +3,28 @@ Create a function named 'debounce',
 it will execute a function after a specified 
 delay without being called again during that delay.
 */
+
+function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+    const context = this;
+
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+}
+
+// example usage
+function logMessage(message: string) {
+  console.log(`Log: ${message}`);
+}
+const debouncedLog = debounce(logMessage, 250);
+// message logged after 250ms delay
+debouncedLog("This is my message");
